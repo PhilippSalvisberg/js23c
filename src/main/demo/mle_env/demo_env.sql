39f30549729fb70d57454cdd79c0aa9702779bb5
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-10-20 19:48:53 +0200

    add language options to demo_env

--- src/main/demo/mle_env/demo_env.sql
+++ src/main/demo/mle_env/demo_env.sql
@@ -2,4 +2,5 @@ create or replace mle env demo_env
     imports(
         'validator' module validator_mod,
         'util' module util
-    );
+    )
+    language options 'js.strict=true, js.console=false, js.polyglot-builtin=true';
