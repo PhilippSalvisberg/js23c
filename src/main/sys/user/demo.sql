078e6869700b7294514bbfa5eba3a5ba7101f52f
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-10-22 17:36:32 +0200

    add create table priv for demo

--- src/main/sys/user/demo.sql
+++ src/main/sys/user/demo.sql
@@ -22,3 +22,6 @@ grant create mle to demo;
 -- for experiments
 grant read on sys.v_$sesstat to demo;
 grant read on sys.v_$statname to demo;
+
+-- for create_temp_table_[plsql|js]
+grant create table to demo;
