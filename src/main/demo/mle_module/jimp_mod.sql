9ba5ff8cdac0701c614f357ed4f5713586ac890b
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-10-24 09:58:14 +0200

    add jimp, sentiment modules which might be interesting in the DB

---
+++ src/main/demo/mle_module/jimp_mod.sql
@@ -0,0 +1,14 @@
+-- requires SQLcl or SQL Developer (does not work in SQL*Plus or other clients)
+set define off
+script
+var url = new java.net.URL("https://esm.run/jimp");
+var content = new java.lang.String(url.openStream().readAllBytes(), 
+                java.nio.charset.StandardCharsets.UTF_8);
+var script = 'set scan off ' + '\n' 
+                + 'create or replace mle module jimp_mod '
+                + 'language javascript as ' + '\n' 
+                + content + "\n"
+                + '/' + "\n";
+sqlcl.setStmt(script);
+sqlcl.run();
+/
