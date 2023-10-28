edc25a1c3a5ab6a6a62650167b9903bcbd63a56f
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-10-28 13:16:56 +0200

    add demo-java-privs.sql

---
+++ src/main/sys/user/demo-java-privs.sql
@@ -0,0 +1,15 @@
+-- Privileges required for mysql-connector-java-5.0.8-bin.jar.
+-- Script is not called by the installation process.
+declare
+   co_user constant all_users.username%type := 'DEMO';
+begin
+   dbms_java.grant_permission(co_user,
+      'SYS:java.net.SocketPermission', '*:1024-65535', 'connect, resolve');
+   dbms_java.grant_permission(co_user, 
+      'SYS:java.io.FilePermission', '/tmp', 'read, write');
+   dbms_java.grant_permission(co_user, 
+      'SYS:java.lang.RuntimePermission', 'getClassLoader', '');
+   dbms_java.grant_permission(co_user, 
+      'SYS:java.lang.RuntimePermission', 'setContextClassLoader', '');
+end;
+/
