5982e68cc0f881e450798bcd9bfc455797bd8f37
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2023-10-29 10:49:12 +0100

    add comment to Util.sql

--- src/main/demo/java/Util.sql
+++ src/main/demo/java/Util.sql
@@ -1,5 +1,6 @@
 -- Might cause a ORA-04031: out of shared memory in java pool.
--- In this case call "alter system set java_pool_size="32M" scope=spfile;" and restart the database
+-- In this case call "alter system set java_pool_size="32M" scope=spfile;" and restart the database.
+-- If the java_pool_size is already >=32M then restart the database to solve the issue.
 create or replace and compile java source named "Util" as
 public class Util {
    public static long toEpoch(java.sql.Timestamp ts) {
