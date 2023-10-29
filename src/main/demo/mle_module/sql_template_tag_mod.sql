876322b228fc48b15843153a6c4a1cf8fcc96c8d
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2023-10-29 10:41:19 +0100

    add sql_template_tag_mod.sql

---
+++ src/main/demo/mle_module/sql_template_tag_mod.sql
@@ -0,0 +1,15 @@
+-- Requires SQLcl or SQL Developer (does not work in SQL*Plus or other clients).
+-- Script is not called by the installation process.
+set define off
+script
+var url = new java.net.URL("https://esm.run/sql-template-tag");
+var content = new java.lang.String(url.openStream().readAllBytes(), 
+                java.nio.charset.StandardCharsets.UTF_8);
+var script = 'set scan off ' + '\n' 
+                + 'create or replace mle module sql_template_tag_mod '
+                + 'language javascript as ' + '\n' 
+                + content + "\n"
+                + '/' + "\n";
+sqlcl.setStmt(script);
+sqlcl.run();
+/
