159ea6866b933b009040350a56581f1bc4e40aa0
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-10-30 12:18:14 +0100

    download browser variant and specific version of jimp

--- src/main/demo/mle_module/jimp_mod.sql
+++ src/main/demo/mle_module/jimp_mod.sql
@@ -2,7 +2,7 @@
 -- Script is not called by the installation process.
 set define off
 script
-var url = new java.net.URL("https://esm.run/jimp");
+var url = new java.net.URL("https://esm.run/jimp@0.22.10/browser/lib/jimp.js");
 var content = new java.lang.String(url.openStream().readAllBytes(), 
                 java.nio.charset.StandardCharsets.UTF_8);
 var script = 'set scan off ' + '\n' 
