c95a40aa72a25a0bf527eaf6d6c7fe67de52a0e0
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-10-30 12:18:29 +0100

    add create_temp_table to demo_env

--- src/main/demo/mle_env/demo_env.sql
+++ src/main/demo/mle_env/demo_env.sql
@@ -1,7 +1,8 @@
 create or replace mle env demo_env
     imports(
-        'increase_salary' module increase_salary_mod,
-        'validator'       module validator_mod,
-        'util'            module util_mod
+        'create_temp_table' module create_temp_table_mod,
+        'increase_salary'   module increase_salary_mod,
+        'validator'         module validator_mod,
+        'util'              module util_mod
     )
     language options 'js.strict=true, js.console=false, js.polyglot-builtin=true';
