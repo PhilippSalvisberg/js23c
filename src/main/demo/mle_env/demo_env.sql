e86784ff1dbbf095be42ff9699f442edd6003c3f
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2023-11-03 00:49:37 +0100

    add sql-assert, sql-template-tag to demo_env

--- src/main/demo/mle_env/demo_env.sql
+++ src/main/demo/mle_env/demo_env.sql
@@ -2,6 +2,8 @@ create or replace mle env demo_env
     imports(
         'create_temp_table' module create_temp_table_mod,
         'increase_salary'   module increase_salary_mod,
+        'sql-assert'        module sql_assert_mod,
+        'sql-template-tag'  module sql_template_tag_mod,
         'validator'         module validator_mod,
         'util'              module util_mod
     )
