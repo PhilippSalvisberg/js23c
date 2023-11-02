06c8aa71bd6bae61932100679df51a5bbf4f59a1
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2023-11-03 00:49:17 +0100

    add sql_assert_mod.sql

---
+++ src/main/demo/mle_module/sql_assert_mod.sql
@@ -0,0 +1,14 @@
+-- Requires SQLcl or SQL Developer (does not work in SQL*Plus or other clients).
+set define off
+script
+var url = new java.net.URL("https://esm.run/sql-assert");
+var content = new java.lang.String(url.openStream().readAllBytes(),
+                java.nio.charset.StandardCharsets.UTF_8);
+var script = 'set scan off ' + '\n'
+                + 'create or replace mle module sql_assert_mod '
+                + 'language javascript as ' + '\n'
+                + content + "\n"
+                + '/' + "\n";
+sqlcl.setStmt(script);
+sqlcl.run();
+/
