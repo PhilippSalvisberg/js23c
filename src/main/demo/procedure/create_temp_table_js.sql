4208372ec476bf2d0303ea7ac21edd901201801a
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2023-11-03 00:51:06 +0100

    use demo_env in create_temp_table_js

--- src/main/demo/procedure/create_temp_table_js.sql
+++ src/main/demo/procedure/create_temp_table_js.sql
@@ -1,3 +1,3 @@
 create or replace procedure create_temp_table_js(in_table_name in varchar2) 
-as mle module create_temp_table_mod signature 'createTempTable(string)';
+as mle module create_temp_table_mod env demo_env signature 'createTempTable(string)';
 /
