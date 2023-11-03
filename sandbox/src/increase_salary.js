38d6878abcb48d90b14327d7b6562fdcd36dd828
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-11-03 10:44:27 +0100

    use sql-assert in increase_salary

--- sandbox/src/increase_salary.js
+++ sandbox/src/increase_salary.js
@@ -1,14 +1,17 @@
-//import oracledb from 'mle-js-oracledb';
-import oracledb from 'oracledb';
+//import oracledb from "mle-js-oracledb";
+import oracledb from "oracledb";
+import { simpleSqlName } from "sql-assert";
 
 export async function increase_salary(deptno, by_percent) {
-    let config = {user: "demo", password: "demo", connectString: "192.168.1.8:51007/freepdb1"};
-    let conn = await oracledb.getConnection(config);
-    let result = await conn.execute(`
-        update emp
-        set sal = sal + sal * :by_percent / 100
-        where deptno = :deptno`, 
-        [by_percent, deptno]
-    );
+    const config = { user: "demo", password: "demo", connectString: "192.168.1.8:51007/freepdb1" };
+    const conn = await oracledb.getConnection(config);
+    // Cannot use sql-template-tag since they use either ? or $ as bind variables.
+    // Both variants are not supported by oracledb. No plans by Oracle to support them.
+    // See https://github.com/oracle/node-oracledb/issues/109
+    const stmt = `
+        update ${simpleSqlName("emp")}
+           set sal = sal + sal * :by_percent / 100
+         where deptno = :deptno`;
+    const result = await conn.execute(stmt, [by_percent, deptno]);
     return result.rowsAffected;
 }
