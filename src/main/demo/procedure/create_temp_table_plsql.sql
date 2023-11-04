abf7c844e192b391e167245b3db1c1161c46e514
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2023-11-04 12:17:23 +0100

    simplify create_temp_table_plsql

--- src/main/demo/procedure/create_temp_table_plsql.sql
+++ src/main/demo/procedure/create_temp_table_plsql.sql
@@ -1,10 +1,8 @@
 -- translation of create_temp_table_mod from JS to PL/SQL with same behavior
 create or replace procedure create_temp_table_plsql(in_table_name in varchar2) is
-   co_templ           constant varchar2(1000 char) :=
+   co_templ constant varchar2(1000 char) :=
       'create private temporary table ora$ptt_#valid_table_name# (id number)';
-   l_valid_table_name user_tables.table_name%type;
 begin
-   l_valid_table_name := dbms_assert.simple_sql_name(in_table_name);
-   execute immediate replace(co_templ, '#valid_table_name#', l_valid_table_name);
+   execute immediate replace(co_templ, '#valid_table_name#', dbms_assert.simple_sql_name(in_table_name));
 end;
 /
