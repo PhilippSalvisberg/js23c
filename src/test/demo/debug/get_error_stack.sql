4b14c8c128812a2465a0a2e6b150b528c60f14bf
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2023-11-04 12:19:13 +0100

    add get_error_stack.sql

---
+++ src/test/demo/debug/get_error_stack.sql
@@ -0,0 +1,40 @@
+create or replace mle module 
+   create_temp_table_mod language javascript as
+
+import { simpleSqlName } from "sql-assert";
+
+export function createTempTable(tableName) {
+  session.execute(
+    `create private temporary table
+        ora\$ptt_${simpleSqlName(tableName)} (id number)`
+  );
+}
+/
+
+exec create_temp_table_js('test a');
+
+set serveroutput on size unlimited
+declare
+   t_frames dbms_mle.error_frames_t;
+begin
+   t_frames := dbms_mle.get_error_stack(
+                  module_name => 'CREATE_TEMP_TABLE_MOD',
+                  env_name    => 'DEMO_ENV'
+               );
+   for i in t_frames.count
+   loop
+      dbms_output.put_line(
+         'ORA-04171: at '
+         || t_frames(i).func
+         || ' ('
+         || t_frames(i).source
+         || ':'
+         || t_frames(i).line
+         || ':'
+         || t_frames(i).col
+         || ')'
+      );
+      null;
+   end loop;
+end;
+/
