a05da09693dcd6b8fd0be1d6bd3209c76d45b49c
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-11-06 11:36:43 +0100

    provide global session variable in test

--- sandbox/src/increase_salary.js
+++ sandbox/src/increase_salary.js
@@ -1,17 +1,6 @@
 import { simpleSqlName } from "sql-assert";
 
 export async function increase_salary(deptno, by_percent) {
-    let conn;
-    try {
-        // works when it runs within the Oracle Database
-        conn = session;
-    } catch (ReferenceError) {
-        // runs outside of the Oracle Database
-        const oracledb = await import("oracledb");
-        const config = { user: "demo", password: "demo", connectString: "192.168.1.8:51007/freepdb1" };
-        conn = await oracledb.getConnection(config);
-    }
-    
     // Cannot use sql-template-tag since they use either ? or $ as bind variables.
     // Both variants are not supported by oracledb. No plans by Oracle to support them.
     // See https://github.com/oracle/node-oracledb/issues/109
@@ -19,6 +8,6 @@ export async function increase_salary(deptno, by_percent) {
         update ${simpleSqlName("emp")}
            set sal = sal + sal * :by_percent / 100
          where deptno = :deptno`;
-    const result = await conn.execute(stmt, [by_percent, deptno]);
+    const result = await session.execute(stmt, [by_percent, deptno]);
     return result.rowsAffected;
 }
--- sandbox/test/demo-increase-salary.js
+++ sandbox/test/demo-increase-salary.js
@@ -1,9 +1,14 @@
 import { increase_salary } from '../src/increase_salary.js';
 import assert from 'assert';
+import oracledb from 'oracledb';
+
+const config = { user: "demo", password: "demo", connectString: "192.168.1.8:51007/freepdb1" };
 
 describe('increase_salary', function () {
   it('By 0 percent for dept 10', async function () {
+    global.session = await oracledb.getConnection(config);
     const affectedRows = await increase_salary(10, 0);
     assert(affectedRows == 3);
+    session.close();
   });
 });
