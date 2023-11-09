b9de11fbcc7ff7ac571956ce8aed115b3ba48b5e
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-11-09 13:20:06 +0100

    use session global variable in increase_salary_mod

--- src/main/demo/mle_module/increase_salary_mod.sql
+++ src/main/demo/mle_module/increase_salary_mod.sql
@@ -1,6 +1,6 @@
 create or replace mle module increase_salary_mod language javascript as
 export function increase_salary(deptno, by_percent) {
-   oracledb.defaultConnection().execute(`
+   session.execute(`
       update emp
          set sal = sal + sal * :by_percent / 100
          where deptno = :deptno`, [by_percent, deptno]);
