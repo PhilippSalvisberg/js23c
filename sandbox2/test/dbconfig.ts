9c415d821f0d7bf89e1b2f7c9b1b4f6cfb29544e
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-11-10 17:04:29 +0100

    add dbconfig.ts

---
+++ sandbox2/test/dbconfig.ts
@@ -0,0 +1,53 @@
+import oracledb from "oracledb";
+
+export let sysSession: oracledb.Connection;
+export let demotabSession: oracledb.Connection;
+export let otheruserSession: oracledb.Connection;
+
+const connectString = "192.168.1.8:51007/freepdb1";
+
+export const sysConfig: oracledb.ConnectionAttributes = {
+    user: "sys",
+    password: "oracle",
+    connectString: connectString,
+    privilege: oracledb.SYSDBA
+};
+
+export const configDemotab: oracledb.ConnectionAttributes = {
+    user: "demotab",
+    password: "demotab",
+    connectString: connectString
+};
+
+export const configOtheruser: oracledb.ConnectionAttributes = {
+    user: "otheruser",
+    password: "otheruser",
+    connectString: connectString
+};
+
+export async function createSessions(): Promise<void> {
+    sysSession = await oracledb.getConnection(sysConfig);
+    await createUser(configDemotab);
+    await createUser(configOtheruser);
+    await sysSession.execute("grant create public synonym to demotab");
+    await sysSession.execute("grant execute on javascript to public");
+    demotabSession = await oracledb.getConnection(configDemotab);
+    otheruserSession = await oracledb.getConnection(configOtheruser);
+}
+
+async function createUser(config: oracledb.ConnectionAttributes): Promise<void> {
+    await sysSession.execute(`drop user if exists ${config.user} cascade`);
+    await sysSession.execute(`
+        create user ${config.user} identified by ${config.password}
+           default tablespace users
+           temporary tablespace temp
+           quota 1m on users
+    `);
+    await sysSession.execute(`grant db_developer_role to ${config.user}`);
+}
+
+export async function closeSessions(): Promise<void> {
+    await sysSession?.close();
+    await demotabSession?.close();
+    await otheruserSession?.close();
+}
