8b2e0e36618dc7d1d3ecc72e3375be359711cf80
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-11-28 16:24:19 +0100

    upe sql-assert 1.0.4

--- sandbox2/deploy.sql
+++ sandbox2/deploy.sql
@@ -1,6 +1,6 @@
 set define off
 script
-var url = new java.net.URL("https://esm.run/sql-assert@1.0.3");
+var url = new java.net.URL("https://esm.run/sql-assert@1.0.4");
 var content = new java.lang.String(url.openStream().readAllBytes(),
                   java.nio.charset.StandardCharsets.UTF_8);
 var script = 'create or replace mle module sql_assert_mod '
--- src/main/demo1/mle_module/sql_assert_mod.sql
+++ src/main/demo1/mle_module/sql_assert_mod.sql
@@ -1,7 +1,7 @@
 -- Requires SQLcl or SQL Developer (does not work in SQL*Plus or other clients).
 set define off
 script
-var url = new java.net.URL("https://esm.run/sql-assert@1.0.3");
+var url = new java.net.URL("https://esm.run/sql-assert@1.0.4");
 var content = new java.lang.String(url.openStream().readAllBytes(),
                 java.nio.charset.StandardCharsets.UTF_8);
 var script = 'create or replace mle module sql_assert_mod '
