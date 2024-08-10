8ec21ace4e2917a0725b09813253c7e42948b0f4
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2024-08-10 21:54:34 +0200

    use new delimiters for inlined MLE modules
    introduced in 23.4, not a string anymore.
    Looks like a string in this case, but it's not.
    
    You can use almost any sequence of characters
    as a delimiter for the JavaScript code.
    The same character sequence must be used
    for the start and the end delimiter.
    Exceptions are the pairs (), [], {} and <>.

--- src/main/demo1/function/to_epoch_js2.sql
+++ src/main/demo1/function/to_epoch_js2.sql
@@ -1,4 +1,4 @@
-create or replace function to_epoch_js2("in_ts" in timestamp) 
-   return number is 
-      mle language javascript q'[return in_ts.valueOf();]';
+create or replace function to_epoch_js2("in_ts" in timestamp)
+   return number is
+      mle language javascript ' return in_ts.valueOf();';
 /
